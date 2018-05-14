6f51cd06c448acda46a9633fd9a5249f138ee09d
masashihamano <masashi.hamano@gmail.com>
2018-05-15 02:49:14 +0900

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,214 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# user = User.new(
+#   id: 2,
+#   name: "ノダジュエリー",
+#   email: "2@hoge.com",
+#   password: "222222",
+#   password_confirmation: "222222"
+# )
+# user.save!
+#
+# Instrument.create!([{
+#   title: "プラチナ ダイヤ フルーレットネックレス",
+#   brand: "スイート１０",
+#   model: "ジェリー・アクセサリー",
+#   description: "ベルギー王国アルベルト2世より勲章受章のハート＆キューピッド ダイヤモンド。スイートテン・ダイヤモンドネックレス。",
+#   condition: "新品",
+#   finish: "Red",
+#   price: "1000000",
+#   image: Rails.root.join("app/assets/images/instrument2.jpg").open,
+#   user_id: user.id
+# }])
+#
+# user = User.new(
+#   id: 3,
+#   name: "梅商基盤店",
+#   email: "3@hoge.com",
+#   password: "333333",
+#   password_confirmation: "333333"
+# )
+# user.save!
+#
+# Instrument.create!([{
+#   title: "本榧七寸八分柾目碁盤 ",
+#   brand: "本榧",
+#   model: "おもちゃ・ゲーム",
+#   description: "囲碁愛好家が憧れる最高級の対局、超厚盤、堂々の風格本榧七寸八分柾目本榧碁盤。",
+#   condition: "新品未使用品",
+#   finish: "Red",
+#   price: "1200000",
+#   image: Rails.root.join("app/assets/images/instrument3.jpg").open,
+#   user_id: user.id
+# }])
+#
+# user = User.new(
+#   id: 4,
+#   name: "松田楽器店",
+#   email: "4@hoge.com",
+#   password: "444444",
+#   password_confirmation: "444444"
+# )
+# user.save!
+#
+# Instrument.create!([{
+#   title: "YAMAHA C Traditional シリーズ C1TD ",
+#   brand: "ヤマハ",
+#   model: "楽器",
+#   description: "世界中の家庭で愛され、多くのピアニストに選ばれてきたCシリーズ。
+# ピアノとしてのトータルバランスを大切にしながら、Cシリーズの優れたタッチ感、美しい音色や深みのある響き、豊かな表現力を受け継ぎました。",
+#   condition: "Used",
+#   finish: "Seafoam",
+#   price: "1400000",
+#   image: Rails.root.join("app/assets/images/instrument4.jpg").open,
+#   user_id: user.id
+# }])
+#
+# user = User.new(
+#   id: 5,
+#   name: "佐賀県大町町まちづくり振興会",
+#   email: "5@hoge.com",
+#   password: "555555",
+#   password_confirmation: "555555"
+# )
+# user.save!
+#
+# Instrument.create!([{
+#   title: "【ふるさと納税】佐賀牛フィレステーキ！総計２３．４ｋｇ【定期便全１３回】",
+#   brand: "佐賀牛",
+#   model: "食品",
+#   description: "佐賀牛とは肉質等級が「Ａ5等級」、「Ａ４等級」、脂肪交雑（サシ）が「BMSのNo.7以上」という最高級のブランド牛であり、今回この佐賀牛で贅沢にも分厚くカットしたフィレステーキをご用意いたしました。
+#
+# ヒレ肉は牛一頭から3％程しかとれない大変希少な牛肉であり、サーロインとは違い、赤身で肉質がきめ細かく、脂肪や筋がほとんどないため、上品で極上の味を堪能できます。",
+#   condition: "冷凍配送",
+#   finish: "Clear",
+#   price: "2000000",
+#   image: Rails.root.join("app/assets/images/instrument5.jpg").open,
+#   user_id: user.id
+# }])
+#
+# user = User.new(
+#   id: 6,
+#   name: "MARCLE ランクル本店",
+#   email: "6@hoge.com",
+#   password: "666666",
+#   password_confirmation: "666666"
+# )
+# user.save!
+#
+# Instrument.create!([{
+#   title: "ランドクルーザー４０",
+#   brand: "トヨタ",
+#   model: "車",
+#   description: "全国陸送対応！ パワステ クーラー シート張替 天井張替 カーペット 新品マッドタイヤ。
+#   別途「諸費用」が必要となります。
+# 「諸費用」には、保険料（自賠責保険料）、税金（自動車重量税、自動車税（又は軽自動車税）、自動車取得税、法定預かり費用（検査登録印紙代、車庫証明申請証紙等）リサイクル預託金相当額（リサイクル預託金相当額を車両本体価格に含めている場合を除く）、登録等に伴う費用（検査・登録手続代行費用、車庫証明手続代行費用）、諸費用にかかる消費税等、購入時に最低限必要な全ての費用を指します。",
+#   condition: "Used",
+#   finish: "Red",
+#   price: "3000000",
+#   image: Rails.root.join("app/assets/images/instrument6.jpg").open,
+#   user_id: user.id
+# }])
+#
+# user = User.new(
+#   id: 7,
+#   name: "ジャックロード",
+#   email: "7@hoge.com",
+#   password: "777777",
+#   password_confirmation: "777777"
+# )
+# user.save!
+#
+# Instrument.create!([{
+#   title: "ROLEX コスモグラフ デイトナ 6265(第3世代モデル)",
+#   brand: "ロレックス",
+#   model: "腕時計",
+#   description: "ねじ込み式プッシュボタン採用により完全防水に。このモデルで手巻きデイトナは生産終了。最終型にあたるこのRef.6265でプッシャーに防水処理を施し、名称もオイスター・コスモグラフ・デイトナとなった。
+#
+# ステンレスベゼルのRef.6265には、黒文字盤に白目玉のバリエーションも存在していた。ムーブメントは前モデル同様のCal.727。手巻きデイトナの最終型として絶大な人気を誇っている。",
+#   condition: "アンティーク",
+#   finish: "Yellow",
+#   price: "9000000",
+#   image: Rails.root.join("app/assets/images/instrument7.jpg").open,
+#   user_id: user.id
+# }])
+
+
+user = User.new(
+  id: 2,
+  name: "Andy Leverenz",
+  email: "andy@example.com",
+  password: "password",
+  password_confirmation: "password"
+)
+user.save!
+
+Instrument.create!([{
+  title: "Paul Reed Smith Paul's Guitar 2013",
+  brand: "PRS",
+  model: "Paul Reed Smith Paul's Guitar",
+  description: "Donec sed odio dui. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+  condition: "Excellent",
+  finish: "Red",
+  price: "2999",
+  image: Rails.root.join("app/assets/images/instrument1.jpg").open,
+  user_id: user.id
+},
+{
+  title: "2017 Gibson Les Paul Standard Bourbon Burst 100% Mint/Unplayed Condition!",
+  brand: "Gibson",
+  model: "Les Paul Standard",
+  description: "Hello and thank you for looking at my Item. We are proud to present this stunning 2018 Gibson Les Paul Standard in Bourbon Burst in 100% Mint/Unplayed condition! The 2017 LP Standards are extremely nice,  with a super comfortable necks, and awesome bold finishes.  This Bourbon Burst finish is absolutely stunning! The tone that comes out of this monster is everything you would expect from a Les Paul Standard!! The guitar sounds awesome and has that famous Les Paul  Tone.  Very easy to play with a nice comfortable standard neck featuring a compound radius fret board which means shredding speed is easier on a Gibson! This is one great guitar for the money!  Will  ship via Fed Ex Ground or Home Delivery in Brand New Gibson case with pictured case candy insured for full purchase price!",
+  condition: "Used",
+  finish: "Red",
+  price: "2595",
+  image: Rails.root.join("app/assets/images/instrument2.jpg").open,
+  user_id: user.id
+},
+{
+  title: "Suhr Classic Antique Pro SSS Limited - Surf Green Over 3 Tone Sunburst",
+  brand: "Suhr",
+  model: "Antique Pro SSS Limited",
+  description: "Our customers have asked for an instrument that has a vintage look and feel, without sacrificing playability and tone. Enter the Classic Antique™. We designed this guitar to ensure it preserves the spirit of a vintage instrument while performing like a Suhr. Impeccable craftsmanship and attention to detail ensure that every neck pocket is tight, every fret is perfectly dressed, and that every instrument is ready for peak performance before leaving our facility.
+The nitro-cellulose lacquer finish and our proprietary antiquing process make each Classic Antique™ feel like it has been loved for years. The Classic Antique now includes: our innovative SSCII (Silent Single-Coil) hum cancelling system, a vintage tinted nitrocellulose neck with stainless steel frets, and a Maple fingerboard option.",
+  condition: "New",
+  finish: "Seafoam",
+  price: "2845",
+  image: Rails.root.join("app/assets/images/instrument3.jpg").open,
+  user_id: user.id
+},
+{
+  title: "Fender American Professional Series Telecaster",
+  brand: "Fender",
+  model: "Telecaster",
+  description: "The Fender American Professional Series Telecaster brings the company's original electric guitar rocketing into the 21st century with a full complement of upgraded electronics and appointments. Sporting a pair of V-Mod Single-Coil pickups designed by guitar sensei Tim Shaw himself, each pup is custom designed for its placement to get the most out of your neck and bridge. For those who want that trebled Tele scream in lower volume settings, have no fear: the new treble bleed circuit standard on the American Pro Series ensures you get that same presence regardless of where you have the volume knob.",
+  condition: "New",
+  finish: "Clear",
+  price: "960",
+  image: Rails.root.join("app/assets/images/instrument4.jpg").open,
+  user_id: user.id
+},
+{
+  title: "Gibson SG Special",
+  brand: "Gibson",
+  model: "SG",
+  description: "The Gibson SG Special Faded was born from a variety of small changes made to Gibson instruments over the course of 50-plus years. The SG Special evolved from the Les Paul Special in the early '60s, and was meant to be a less expensive, stripped-down version of the SG Standard for no-frills players. The SG Special Faded, released in 2002 and still in production today, implemented another cost-reducing measure: the use of a light satin finish. Those digging the SG body shape but who don't need unnecessary bells and whistles may want to take a look at the Gibson SG Special Faded. ",
+  condition: "Used",
+  finish: "Red",
+  price: "599",
+  image: Rails.root.join("app/assets/images/instrument5.jpg").open,
+  user_id: user.id
+},
+{
+  title: "Ibanez PM20 Pat Metheny Signature + Hard Shell case",
+  brand: "Ibanez",
+  model: "SG",
+  description: "Path Metheny Signature model guitar in excellent condition. Near zero fret wear and electronics work as expected. Gorgeous natural finish and high quality materials. Back looks amazing as well. No buzz nor issues, low action and very nice tone.",
+  condition: "Used",
+  finish: "Yellow",
+  price: "799",
+  image: Rails.root.join("app/assets/images/instrument6.jpg").open,
+  user_id: user.id
+}])
